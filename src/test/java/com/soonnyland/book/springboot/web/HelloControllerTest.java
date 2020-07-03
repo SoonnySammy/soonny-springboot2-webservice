3c767da2a02581e16caaa28a3f5d3a23c35b0d4a
SoonnySammy <soonnysammy@gmail.com>
2020-07-03 20:17:12 +0900

    Fix import package in HelloControllerTest

--- src/test/java/com/soonnyland/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/soonnyland/book/springboot/web/HelloControllerTest.java
@@ -9,7 +9,9 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
